e1408ebb926dd1a32ffcda3a9211d50dc009df1f
Jim Kang <jimkang@gmail.com>
2024-09-28 22:51:16 -0400

    Vertical border crossing case.

--- line-hit-rect.js
+++ line-hit-rect.js
@@ -15,6 +15,22 @@ export function lineHitRect({ line, rect }) {
     return true;
   }
 
+  const m = getSlope(line);
+  console.log('m', m);
+  const b = getIntercept(m, line.pt1[0], line.pt1[1]);
+  console.log('b', b);
+
+  if (isInBounds(calcLineY(m, rect.left, b), rect.top, rect.bottom)) {
+    // The line hits the left side of the rect.
+    return true;
+  }
+  if (isInBounds(calcLineY(m, rect.right, b), rect.top, rect.bottom)) {
+    // The line hits the right side of the rect.
+    return true;
+  }
+
+  return false;
+
   function pointIsInsideRect(pt) {
     if (pt[0] < rect.left) {
       return false;
@@ -30,4 +46,25 @@ export function lineHitRect({ line, rect }) {
     }
     return true;
   }
+}
+
+function getSlope(line) {
+  const dx = line.pt2[0] - line.pt1[0];
+  const dy = line.pt2[1] - line.pt1[1];
+  if (dx === 0) {
+    return undefined;
+  }
+  return dy / dx;
+}
+
+function getIntercept(m, x, y) {
+  return y - m * x;
+}
+
+function calcLineY(m, x, b) {
+  return m * x + b;
+}
+
+function isInBounds(n, low, high) {
+  return n >= low && n <= high;
 }
